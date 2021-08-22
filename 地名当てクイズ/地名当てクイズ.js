4add950549e3a3642872d432b0b43bbd29498826
あなたの名前 <>
2021-08-22 22:18:45 +0900

    week6

--- 地名当てクイズ/地名当てクイズ.js
+++ 地名当てクイズ/地名当てクイズ.js
@@ -26,15 +26,29 @@ const images = [
     ["https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png", "鹿骨"],
     ["https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png", "小榑"],
 ];
+const shuffle = ([...array]) => {
+  for (let k = array.length - 1; k >= 0; k--) {
+    const j = Math.floor(Math.random() * (k + 1));
+    [array[k], array[j]] = [array[j], array[k]];
+  }
+  return array;
+};
+const array = [0, 1, 2];
+ 
+const random = shuffle(array);
+const random0= random[0];
+const random1= random[1];
+const random2= random[2];
+
 for (let i = 0; i < images.length; i++) {
     let quizySet =
-        `<div>`
+        `<div class = container>`
         + `<h1 class="question">${i + 1}.この地名はなんて読む？</h1>`
         + `</div>`
         + `<img src = ${images[i][0]} alt = ${images[i][1]} class="image">`
-        + `<li class="choice" id = "right${i}">${choices[i][0]}</li>`
-        + `<li class="choice" id = "wrong${i}-1">${choices[i][1]}</li>`
-        + `<li class="choice" id = "wrong${i}-2">${choices[i][2]}</li>`
+        + `<li class="choice" id = "choice${i}-${random[0]}">${choices[i][random[0]]}</li>`
+        + `<li class="choice" id = "choice${i}-${random[1]}">${choices[i][random[1]]}</li>`
+        + `<li class="choice" id = "choice${i}-${random[2]}">${choices[i][random[2]]}</li>`
         + `<div class="resultBox" id="correctResultBox${i}">`
         + `<p  class = "correct" id="correct${i}" >正解！</p>`
         + `<p>正解は「${choices[i][0]}」です！</p>`
@@ -44,33 +58,34 @@ for (let i = 0; i < images.length; i++) {
         + `<p>正解は「${choices[i][0]}」です！</p>`
         + `</div>`
     document.write(quizySet);
-    document.getElementById(`right${i}`).addEventListener('click', () => {
-        document.getElementById(`right${i}`).style.background = 'blue';
-        document.getElementById(`right${i}`).style.color = 'white';
+
+    document.getElementById(`choice${i}-0`).addEventListener('click', () => {
+        document.getElementById(`choice${i}-0`).style.background = 'blue';
+        document.getElementById(`choice${i}-0`).style.color = 'white';
         document.getElementById(`correctResultBox${i}`).style.display = "block";
         document.getElementById(`correct${i}`).style.display = "block";
-        document.getElementById(`wrong${i}-1`).classList.add("click-block");
-        document.getElementById(`wrong${i}-2`).classList.add("click-block");
+        document.getElementById(`choice${i}-1`).classList.add("click-block");
+        document.getElementById(`choice${i}-2`).classList.add("click-block");
     })
-    document.getElementById(`wrong${i}-1`).addEventListener('click', () => {
-        document.getElementById(`right${i}`).style.background = 'blue';
-        document.getElementById(`right${i}`).style.color = 'white';
+    document.getElementById(`choice${i}-1`).addEventListener('click', () => {
+        document.getElementById(`choice${i}-0`).style.background = 'blue';
+        document.getElementById(`choice${i}-0`).style.color = 'white';
         document.getElementById(`incorrectResultBox${i}`).style.display = "block";
         document.getElementById(`incorrect${i}`).style.display = "block";
-        document.getElementById(`wrong${i}-1`).style.background = 'red';
-        document.getElementById(`wrong${i}-1`).style.color = 'white';
-        document.getElementById(`right${i}`).classList.add("click-block");
-        document.getElementById(`wrong${i}-2`).classList.add("click-block");
+        document.getElementById(`choice${i}-1`).style.background = 'red';
+        document.getElementById(`choice${i}-1`).style.color = 'white';
+        document.getElementById(`choice${i}-0`).classList.add("click-block");
+        document.getElementById(`choice${i}-2`).classList.add("click-block");
     })
-    document.getElementById(`wrong${i}-2`).addEventListener('click', () => {
-        document.getElementById(`right${i}`).style.background = 'blue';
-        document.getElementById(`right${i}`).style.color = 'white';
+    document.getElementById(`choice${i}-2`).addEventListener('click', () => {
+        document.getElementById(`choice${i}-0`).style.background = 'blue';
+        document.getElementById(`choice${i}-0`).style.color = 'white';
         document.getElementById(`incorrectResultBox${i}`).style.display = "block";
         document.getElementById(`incorrect${i}`).style.display = "block";
-        document.getElementById(`wrong${i}-2`).style.background = 'red';
-        document.getElementById(`wrong${i}-2`).style.color = 'white';
-        document.getElementById(`right${i}`).classList.add("click-block");
-        document.getElementById(`wrong${1}-1`).classList.add("click-block");
+        document.getElementById(`choice${i}-2`).style.background = 'red';
+        document.getElementById(`choice${i}-2`).style.color = 'white';
+        document.getElementById(`choice${i}-0`).classList.add("click-block");
+        document.getElementById(`choice${1}-1`).classList.add("click-block");
     })
 }
 
