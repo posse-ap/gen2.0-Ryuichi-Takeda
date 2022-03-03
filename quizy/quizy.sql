9343e9b5d3a7c7e0f943582c3acac3f64d6f97b1
あなたの名前 <>
2022-03-03 13:45:32 +0900

    保存

--- quizy/quizy.sql
+++ quizy/quizy.sql
@@ -1,47 +1,11 @@
-DROP TABLE IF EXISTS choices;
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS prefectures;
+DROP TABLE IF EXISTS big_questions;
 
 
-CREATE TABLE prefectures (
-    id INT NOT NULL AUTO_INCREMENT,
-    prefecture VARCHAR(20), 
-    PRIMARY KEY (id)
-);
+CREATE TABLE `big_questions`(
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT '県ID',
+    `prefecture_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '県名'
+)
 
-CREATE TABLE questions (
-    id INT NOT NULL AUTO_INCREMENT,
-    prefecture_id INT,
-    question VARCHAR(20), 
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE choices (
-    id INT NOT NULL AUTO_INCREMENT,
-    question_id INT,
-    choice VARCHAR(20), 
-    result  VARCHAR(20),
-    PRIMARY KEY (id),
-    FOREIGN KEY (question_id) REFERENCES questions(id)
-);
-
-INSERT INTO prefectures(prefecture) VALUES
+INSERT INTO big_questions (prefecture_name) VALUES
     ('東京'),
     ('広島');
-    
-INSERT INTO questions(prefecture_id,question) VALUES
-    (1,'高輪'),
-    (1,'亀戸');
-
-INSERT INTO choices(question_id,choice,result) VALUES
-    (1,'たかなわ',TRUE),
-    (1,'たかわ',FALSE),
-    (1,'こうわ',FALSE),
-    (2,'かめいど',TRUE),
-    (2,'かめど',FALSE),
-    (2,'かめと',FALSE);
-    
-    SELECT * FROM prefectures;
-    SELECT * FROM questions;
-    SELECT * FROM choices;
--- week29
